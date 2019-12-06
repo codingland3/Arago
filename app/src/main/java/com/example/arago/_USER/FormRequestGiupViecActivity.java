5fb312455bbc52da4037a8834e9a9d83c6949c33
Trung Kiên <kiennt-LT@users.noreply.github.com>
2019-12-07 02:44:19 +0700

    .

---
+++ app/src/main/java/com/example/arago/_USER/FormRequestGiupViecActivity.java
@@ -0,0 +1,21 @@
+package com.example.arago._USER;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.arago.R;
+
+public class FormRequestGiupViecActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_request_giupviec);
+    }
+
+    public void clickBack(View view) {
+        finish();
+    }
+}
