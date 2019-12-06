21bccf30199ffcf8abab528c20f87b5d900482ba
Trung Kiên <kiennt-LT@users.noreply.github.com>
2019-12-06 09:12:39 +0700

    Update Partner Firebase

--- app/src/main/java/com/example/arago/USER/Model/Request.java
+++ app/src/main/java/com/example/arago/USER/Model/Request.java
@@ -18,6 +18,7 @@ public class Request {
         this.request_brand = request_brand;
         this.request_datetime = request_datetime;
     }
+    //
 
     public int getRequest_id() {
         return request_id;
